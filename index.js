046507eb711e994afcd6bff2a0e834e2cfc61681
Tim Miller <millman@icloud.com>
2019-05-14 11:39:01 -0400

    Convert to TypeScript

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-var critical = "critical";
-var high = "high";
-var medium = "medium";
-var low = "low";
+var Severity;
+(function (Severity) {
+    Severity["critical"] = "critical";
+    Severity["high"] = "high";
+    Severity["medium"] = "medium";
+    Severity["low"] = "low";
+})(Severity || (Severity = {}));
 var state = [
     {
-        eventName: "Critical Condition",
+        name: "Critical Condition",
         id: Math.random(),
-        severity: critical
+        severity: Severity.critical
     },
     {
-        eventName: "Medium Condition",
+        name: "Medium Condition",
         id: Math.random(),
-        severity: medium
+        severity: Severity.medium
     }
 ];
 for (var x in state) {
@@ -19,7 +22,7 @@ for (var x in state) {
 }
 function addEventItem(item) {
     var el = document.createElement("li");
-    el.textContent = item.eventName;
+    el.textContent = item.name;
     el.classList.add(item.severity);
     document.getElementById("eventList").appendChild(el);
 }
@@ -28,9 +31,9 @@ var form = document.getElementById("addForm").addEventListener("submit", functio
     var severityInput = document.getElementById("severity");
     var eventNameInput = document.getElementsByName("eventName")[0];
     var newItem = {
-        eventName: eventNameInput.value,
+        name: eventNameInput.value,
         id: Math.random(),
-        severity: severityInput.options[severityInput.selectedIndex].value
+        severity: severityInput.options[severityInput.selectedIndex].text
     };
     state.push(newItem);
     addEventItem(newItem);
